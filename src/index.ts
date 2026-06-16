01390bfb9dfee2b156c43f2b8bf2a58e0682f2d7
insane6l <43768644+insane4l@users.noreply.github.com>
2026-06-16 22:46:31 +0300

    chore: set up Heroku deploy: add scripts to package json + add port variable from env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3006;
+const port = process.env.PORT || 3006;
 const JsonBodyMiddleware = express.json();
 app.use(JsonBodyMiddleware);
 
